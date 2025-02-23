87a6115a7a554aacd06cabba2a0ae0b32b218ffb
AbhinavTuladhar <tuladharabhinav78@gmail.com>
2025-02-23 19:57:21 +0545

    feat: make title font size fluid

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -5,14 +5,14 @@ const Header = () => {
     <header className="content-grid bg-primary-foreground border-b border-b-slate-500 py-4">
       <div className="feature flex items-center justify-between">
         <Link to="/">
-          <h1 className="text-3xl font-bold"> CSS Fluid Size Calculator</h1>
+          <h1 className="text-heading font-bold"> CSS Fluid Size Calculator</h1>
         </Link>
-        <Link
+        {/* <Link
           to="/about"
           className="rounded border border-slate-400 px-4 py-2 duration-300 hover:bg-slate-100 hover:text-black"
         >
           About
-        </Link>
+        </Link> */}
       </div>
     </header>
   )
